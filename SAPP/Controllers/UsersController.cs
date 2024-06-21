d28cb59cf62b825ca3d83c9461b90bdf5e2742e7
Juan Guio <juanpabloguio@gmail.com>
2024-06-21 14:17:34 -0500

    se agrega la carpeta SAPP

---
+++ SAPP/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SAPP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        
+        
+    }
+}
