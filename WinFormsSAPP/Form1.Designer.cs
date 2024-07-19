27db87555a8d3f63fe42e71cfb6d42993d8a1f22
Fredy Guio <fredyguio93@gmail.com>
2024-07-19 12:51:42 -0500

    Se inicia el proyecto de windowsform

---
+++ WinFormsSAPP/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinFormsSAPP
+{
+    partial class InterfazSAPP
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.BackColor = Color.DeepSkyBlue;
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(-2, -10);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1139, 80);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(528, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 28);
+            label1.TabIndex = 0;
+            label1.Text = "SAPP";
+            label1.Click += label1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            groupBox2.BackColor = Color.White;
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Location = new Point(8, 80);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(466, 567);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(150, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(154, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Registro de Usuarios";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(16, 83);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(19, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Primer Nombre";
+            // 
+            // InterfazSAPP
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1136, 659);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "InterfazSAPP";
+            Text = "SAPP";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+    }
+}
---
+++ WinFormsSAPP/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsSAPP
+{
+    public partial class InterfazSAPP : Form
+    {
+        public InterfazSAPP()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
